0d7e683ba4cf59888e98cc72e240895f40141116
Martin Taillefer <geeknoid@users.noreply.github.com>
2020-03-05 22:53:40 -0800

    Undo previous unnecessary changes.

--- scripts/build_archive_site.sh
+++ scripts/build_archive_site.sh
@@ -56,13 +56,13 @@ for rel in "${TOBUILD[@]}"; do
   echo "### Building '${NAME}' from ${TAG} for ${URL}"
   git checkout "${TAG}"
 
-  if [[ "${TAG}" == "release-1.2" || "${TAG}" == "release-1.3" || "${TAG}" == "release-1.4" ]]; then
+  if [[ "${TAG}" == "release-1.2" || "${TAG}" == "release-1.3" ]]; then
     scripts/build_site.sh
   fi
 
   scripts/gen_site.sh "${URL}"
 
-  if [[ "${TAG}" != "release-0.8" && "${TAG}" != "release-1.0" && "${TAG}" != "release-1.1" && "${TAG}" != "release-1.2" && "${TAG}" != "release-1.3" && "${TAG}" != "release-1.4" ]]; then
+  if [[ "${TAG}" != "release-0.8" && "${TAG}" != "release-1.0" && "${TAG}" != "release-1.1" && "${TAG}" != "release-1.2" && "${TAG}" != "release-1.3" ]]; then
     scripts/build_site.sh
   fi
 
